8f9cdbd537bc9e0a03ed8131f132cbac8f1be16f
mijail-febres <mijail.febres@gmail.com>
2021-11-23 22:28:15 +0100

    HashRouter inside App

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import './App.css';
 import { Route, Routes} from 'react-router-dom';
 import Home from './Pages/Home/Home';
 import About from './Pages/About/About';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="about" element={<About/>} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
