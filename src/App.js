521c596beed50ef44e9d3a7563e401ed8cf6b304
mijail-febres <mijail.febres@gmail.com>
2021-11-23 21:11:26 +0100

    home

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App = () => {
     // <BrowserRouter>
     // <HashRouter>
       <Routes>
-        <Route path='/' element={<Home/>} />
+        <Route path='/home' element={<Home/>} />
         <Route path='/about' element={<About/>} />
       </Routes>
     // </HashRouter>
