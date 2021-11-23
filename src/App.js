6813516264b52207fc6997d53ea2c98d1aca8eba
mijail-febres <mijail.febres@gmail.com>
2021-11-23 21:27:21 +0100

    eliminate non-used imports

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter, HashRouter, Route, Routes} from 'react-router-dom';
+import { Route, Routes} from 'react-router-dom';
 import Home from './Pages/Home/Home';
 import About from './Pages/About/About';
 
